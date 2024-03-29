3455651641334e7d4cf91024d8746d4bd9aff220
KityLinn <87649871+KityLinn@users.noreply.github.com>
2024-03-29 13:01:12 +0100

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-
+import {Routes, Route} from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Contact } from "./pages/Contact"
+import { Product } from "./pages/Product"
+import { Checkout } from "./pages/Checkout"
+import { CheckoutSuccess } from "./pages/CheckoutSuccess"
 
 function App() {
  
 
   return (
     <>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/product" element={<Product/>}/>
+        <Route path="/checkout" element={<Checkout/>}/>
+        <Route path="/checkoutsuccess" element={<CheckoutSuccess/>}/>
+      </Routes>
+    </Container>
     </>
   )
 }
