00ca46c2fcf7d2b8603d0c2e42d49538219cb0b4
KityLinn <87649871+KityLinn@users.noreply.github.com>
2024-04-02 15:19:11 +0200

    backup

---
+++ src/components/Storeitem.tsx
@@ -0,0 +1,11 @@
+import { Button, Card } from "react-bootstrap"
+
+type StoreItemProps = {
+    description: string
+  }
+
+export function StoreItem({ description }: StoreItemProps) {
+    <h1>{description}</h1>
+
+
+ }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,29 @@
 import { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../components/Storeitem";
 
 export function Home() {
     const url =  "https://v2.api.noroff.dev/online-shop"
-    const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState<any[]>([])
   useEffect(() => {
     async function getData() {
       const response = await fetch(url);
       const json = await response.json();
-      setPosts(json);
+      setPosts(json.data);
     }
     getData()
-    
-  }, []);
+      
+  }, []); 
+
+  console.log(posts[1].title)
 
 
     return (
     <>
     <h1>My Store</h1>
     <Row lg ={3} md = {2} xs = {1}>
+   
+
     </Row>
     
     </>
