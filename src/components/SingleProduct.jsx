6d13e7d13db099af24d5a91d5e667d35700307ba
KityLinn <87649871+KityLinn@users.noreply.github.com>
2024-04-03 22:49:16 +0200

    everything works again

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,8 +1,12 @@
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Row, Col } from "react-bootstrap";
 
 export function SingleProduct({ title, id, description, image, price, discountedPrice, rating, reviews}) {
     return (
         <>
+        <Row max-width = "600px">
+            <img src= {image.url} alt="" />
+
+        </Row>
         
         </>
     )
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,24 +1,26 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { SingleProduct } from "../components/SingleProduct"
+import { Col, Row } from "react-bootstrap";
+import { SingleProduct } from "../components/SingleProduct.jsx";
+
 
 export function Product() {
-    const url =  "https://v2.api.noroff.dev/online-shop/?"
-    const [posts, setPosts] = useState([])
-    const {id} = useParams()
+  const url = "https://v2.api.noroff.dev/online-shop/";
+  const { id } = useParams();
+  const [posts, setPosts] = useState([]);
   useEffect(() => {
     async function getData() {
       const response = await fetch(url + id);
       const json = await response.json();
       setPosts(json.data);
     }
-    getData()
-    console.log(posts)
-      
-  }, []); 
-    return (
-      <>
-      <SingleProduct {...posts} />
-      </>
-    )
+    getData();
+  }, []);
+  console.log(posts);
+
+  return (
+    <>
+    <h1>Test</h1>
+    </>
+  );
 }
