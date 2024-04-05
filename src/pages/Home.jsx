908e09f5be79186260d7c3c2b35acce6db2760a5
KityLinn <87649871+KityLinn@users.noreply.github.com>
2024-04-05 18:18:23 +0200

    search in progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Form} from "react-bootstrap";
 import { StoreItem } from "../components/Storeitem.jsx";
 
 export function Home() {
   const url = "https://v2.api.noroff.dev/online-shop";
   const [posts, setPosts] = useState([]);
+  const [search, setSearch] = useState("");
   useEffect(() => {
     async function getData() {
       const response = await fetch(url);
@@ -15,9 +16,30 @@ export function Home() {
   }, []);
   console.log(posts);
 
+
+ function searchFunc (e) {
+  return setSearch(e.target.value)
+
+ }
+
+ console.log(search)
+ 
+
   return (
     <>
       <h1 className="mb-5">My Store</h1>
+      <Form.Group
+        className="d-flex flex-column align-items-center justify-content-center mb-5"
+        controlId="searchForm"
+      >
+        <Form.Label>Search here</Form.Label>
+        <Form.Control
+          className="w-50 border-black border-2"
+          type="text"
+          placeholder="Search.."
+          onChange={searchFunc}
+        />
+      </Form.Group>
       <Row xs={1} md={2} lg={3} className="g-3">
         {posts.map((items) => {
           return (
