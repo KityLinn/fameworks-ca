e9d34b4a836ca61a78cf82f4cbee95981aa53917
KityLinn <87649871+KityLinn@users.noreply.github.com>
2024-04-06 23:56:41 +0200

    cart item card with new prices

--- src/components/Cartitem.jsx
+++ src/components/Cartitem.jsx
@@ -5,19 +5,20 @@ export function Cartitem({items:{item, amount}}) {
 
     return (
       <>
-        <Card key={item.id} className="h-100" style={{ maxWidth: "300px" }}>
+        <Card key={item.id} className="h-100 d-flex flex-row" style={{ maxWidth: "400px" }}>
           <Card.Img
             variant="top"
-            height="250px"
+            height="100px"
             src={item.image?.url}
             alt={item.image?.alt}
             style={{ objectFit: "cover" }}
           />
           <Card.Body className="d-flex flex-column">
-            <Card.Title className="d-flex mb-4 flex-column">
-              <span className="fs-5 mb-5">{item.title}</span>
-              <span className="fs-5">Price: {item.price}kr </span>
+            <Card.Title className="d-flex flex-row gap-5">
+              <span className="fs-6">{item.title}</span>
+              <span className="fs-6">Price: {item.discountedPrice*amount}kr </span>
             </Card.Title>
+            <span>Amount: {amount}</span>
           </Card.Body>
         </Card>
       </>
