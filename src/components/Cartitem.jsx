8ad6eb46dd7016e54ab4c54740945bd1fc95a7fc
KityLinn <87649871+KityLinn@users.noreply.github.com>
2024-04-06 20:49:13 +0200

    changed checkout and caritem to take advantage of new local storage data

--- src/components/Cartitem.jsx
+++ src/components/Cartitem.jsx
@@ -1,21 +1,22 @@
 import { Card, Col } from "react-bootstrap";
 
 
-export function Cartitem({ items }) {
+export function Cartitem({items:{item, amount}}) {
+
     return (
       <>
-        <Card key={items.data.id} className="h-100" style={{ maxWidth: "300px" }}>
+        <Card key={item.id} className="h-100" style={{ maxWidth: "300px" }}>
           <Card.Img
             variant="top"
             height="250px"
-            src={items.data.image?.url}
-            alt={items.data.image?.alt}
+            src={item.image?.url}
+            alt={item.image?.alt}
             style={{ objectFit: "cover" }}
           />
           <Card.Body className="d-flex flex-column">
             <Card.Title className="d-flex mb-4 flex-column">
-              <span className="fs-5 mb-5">{items.data.title}</span>
-              <span className="fs-5">Price: {items.data.price}kr </span>
+              <span className="fs-5 mb-5">{item.title}</span>
+              <span className="fs-5">Price: {item.price}kr </span>
             </Card.Title>
           </Card.Body>
         </Card>
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,26 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import { Cartitem } from "../components/Cartitem.jsx"
  
 export function Checkout() {
-    const [cartData, setcartData] = useState(() => {
-        const data = localStorage.getItem("CARTITEMS")
-        if (data == null) return []
-    
-        return JSON.parse(data)
-      })
+  const [cartData, setcartData] = useState(() => {
+    const data = localStorage.getItem("CARTITEMS")
+    if (data == null) return []
 
-      console.log(cartData)
+    return JSON.parse(data)
+  })
 
-    return (
-        <>
-        <Row className="d-flex flex-column justify-content-center align-items-center mt-5 g-3 w-100">
-        {cartData.map((items) => {
-          return (
-              <Cartitem items={items} />
-          );
-        })}
-        </Row>
-        </>
-      );
+  useEffect(()=> {
+    function storageEventHandler() {
+      let cartString = localStorage.getItem("CARTITEMS");
+      if (cartString) {
+        let cart = JSON.parse(cartString);
+        setcartData(cart);
+      }
+    }
+    storageEventHandler();
+    window.addEventListener("storage", storageEventHandler);
+    return () => {
+        window.removeEventListener("storage", storageEventHandler);
+    };
+  }, []);
+
+  return (
+    <>
+      <Row className="d-flex flex-column justify-content-center align-items-center mt-5 g-3 w-100">
+      {Object.keys(cartData).map((key) => {
+        return (
+            <Cartitem items={cartData[key]} key={key}/>
+        );
+      })}
+      </Row>
+    </>
+  );
 }
