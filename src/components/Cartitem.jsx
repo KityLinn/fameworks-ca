3e01e86e07d4876a5b1c0aed21b0ddcb0230e2f3
KityLinn <87649871+KityLinn@users.noreply.github.com>
2024-04-07 18:11:00 +0200

    created cart total price

--- src/components/Cartitem.jsx
+++ src/components/Cartitem.jsx
@@ -5,7 +5,7 @@ export function Cartitem({items:{item, amount}}) {
 
     return (
       <>
-        <Card key={item.id} className="h-100 d-flex flex-row border-black border-2 p-3" style={{ maxWidth: "400px" }}>
+        <Card key={item.id} className="h-100 d-flex flex-row border-black border-2 p-1" style={{ maxWidth: "400px" }}>
           <Card.Img
             variant="top"
             height="140px"
@@ -16,7 +16,7 @@ export function Cartitem({items:{item, amount}}) {
           <Card.Body className="d-flex flex-column">
             <Card.Title className="d-flex flex-row gap-5">
               <span className="fs-6">{item.title}</span>
-              <span className="fs-6">Price: {item.discountedPrice*amount}kr </span>
+              <span className="fs-6">Total Price: {item.discountedPrice*amount}kr </span>
             </Card.Title>
             <span>Amount: {amount}</span>
           </Card.Body>
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
 import { Cartitem } from "../components/Cartitem.jsx"
  
 export function Checkout() {
@@ -25,17 +25,31 @@ export function Checkout() {
     };
   }, []);
 
+  const [cartTotal, setcartTotal] = useState(0)
+
+  useEffect(()=> {
+    let tempTotal = 0;
+    Object.keys(cartData).forEach(key => {
+    tempTotal +=  cartData[key].amount * cartData[key].item.discountedPrice
+    })
+    setcartTotal(tempTotal)
+  }, [cartData]);
+
+
   console.log(cartData)
 
   return (
     <>
+      <h1>Your Cart</h1>
       <Row className="d-flex flex-column justify-content-center align-items-center mt-5 g-3">
-      {Object.keys(cartData).map((key) => {
-        return (
-            <Cartitem items={cartData[key]} key={key}/>
-        );
-      })}
-      <div>test</div>
+        {Object.keys(cartData).map((key) => {
+          return <Cartitem items={cartData[key]} key={key} />;
+        })}
+        <div className=" d-flex flex-column align-items-center justify-content-center">
+          <h2 className="fs-5">Your Cart Total</h2>
+          <span><p className="fw-bold fs-6">Your total: {cartTotal}</p></span>
+        </div>
+        <Button href= {"/checkoutsuccess"} className="mt-5 p-2" variant="primary" style={{ maxWidth: "200px" }}>Checkout product</Button>
       </Row>
     </>
   );
