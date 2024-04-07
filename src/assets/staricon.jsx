6ac20babdb511e0215c73558f6a69905e1a987b1
KityLinn <87649871+KityLinn@users.noreply.github.com>
2024-04-07 16:57:18 +0200

    imported star icon, changed star icon and added reviews to single product

--- src/assets/staricon.jsx
+++ src/assets/staricon.jsx
@@ -1,9 +1,9 @@
 export function Staricon () {
-    <svg
+    return <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
+      width="1.2rem"
+      height="1.2rem"
+      viewBox="0 1 24 26"
       fill="none"
       stroke="currentColor"
       strokeWidth="2"
--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,39 +1,69 @@
 import { Button, Card, Row, Col } from "react-bootstrap";
+import { Staricon } from "../assets/staricon"
 
 export function SingleProduct({ title, id, description, image, price, discountedPrice, rating, reviews, posts, addToCart}) {
-    return (
-      <>
-        <Card className="h-100" style={{ maxWidth: "600px" }}>
-          <Card.Img
-            variant="top"
-            height="300px"
-            src={image?.url}
-            alt={image?.alt}
-            style={{ objectFit: "cover" }}
-          />
-          <Card.Body className="d-flex flex-column">
-            <Card.Title className="d-flex mb-4 flex-column">
-              <span className="fs-5 mb-5">{title}</span>
-              <span className="fs-5">Price: {discountedPrice?discountedPrice:price}kr </span>
-              {discountedPrice && discountedPrice!==price && 
+  return (
+    <>
+      <Card className="h-100 px-0" style={{ maxWidth: "600px" }}>
+        <Card.Img
+          variant="top"
+          height="300px"
+          src={image?.url}
+          alt={image?.alt}
+          style={{ objectFit: "cover" }}
+        />
+        <Card.Body className="d-flex flex-column">
+          <Card.Title className="d-flex mb-4 flex-column">
+            <span className="fs-5 mb-5">{title}</span>
+            <span className="fs-5">Price: {discountedPrice?discountedPrice:price}kr </span>
+            {discountedPrice && discountedPrice!==price && 
+            <>
+              <span className="fs-6 text-decoration-line-through text-muted">Normal Price: {price}kr</span>
+              <span className="fs-4 text-danger font-weight-bold">Save: {price-discountedPrice}kr</span>
+            </>
+            }
+          </Card.Title>
+          <Card.Text>{description}</Card.Text>
+          <Button
+            className="mt-auto"
+            variant="primary"
+            onClick={() => addToCart(posts)}
+          >
+            Add to cart
+          </Button>
+        </Card.Body>
+        <Card.Footer>
+          <Col>
+            <Row>
+              <span className="fs-5"><Staricon />{rating}/10</span>
+            </Row>
+            {reviews && (
               <>
-                <span className="fs-6 text-decoration-line-through text-muted">Normal Price: {price}kr</span>
-                <span className="fs-4 text-red text-bold">Save: {price-discountedPrice}kr</span>
+                <Row>
+                  <span className="fs-5">Reviews:</span>
+                </Row>
+                {
+                
+                  reviews.map(r=> {
+                  return (
+                    <>
+                    <hr></hr>
+                      <Row>
+                        <Col><span className="fw-bold">{r.username}</span></Col><Col className="d-flex justify-content-end"><span><Staricon />{r.rating}/10</span></Col>
+                      </Row>
+                      <Row>
+                        <Col>{r.description}</Col>
+                      </Row>
+                    </>
+                  );})
+                }
               </>
-              }
-            </Card.Title>
-            <Card.Text>{description}</Card.Text>
-            <Button
-              className="mt-auto"
-              variant="primary"
-              onClick={() => addToCart(posts)}
-            >
-              Add to cart
-            </Button>
-          </Card.Body>
-        </Card>
-      </>
-    );
+            )}
+          </Col>
+        </Card.Footer>
+      </Card>
+    </>
+  );
 
 
 }
